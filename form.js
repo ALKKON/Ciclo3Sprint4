38dfe91a608cc5be033b13e4885e4e3967d19a2d
Ing. Germán Olaya <86852832+andrengineer@users.noreply.github.com>
2021-09-21 23:34:04 -0500

    Commit inicio de validacion formulario de registro

---
+++ form.js
@@ -0,0 +1,8 @@
+// Validar formulario de registro
+
+
+module.exports = checkNombre;
+module.exports = checkTelefono;
+module.exports = checkDir;
+module.exports = checkCorreo;
+module.exports = checkContrasena;
