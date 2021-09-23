c806867809feebc2b53a9be00ce575510d657edd
Ing. Germán Olaya <86852832+andrengineer@users.noreply.github.com>
2021-09-23 00:15:52 -0500

    Commit inicio de validacion formulario de registro

--- form.js
+++ form.js
@@ -1,5 +1,9 @@
 
-// Validar correo electrónico
+/**
+ * Función del módulo form.js para validar correo electrónico
+ * @param {object} input_correo Objeto tipo input de tipo email
+ * @returns {boolean} Booleano de validación
+ */
 function checkCorreo(input_correo) {
     let formato_correo = /^[a-zA-Z0-9À-ÿ.+=_~-]+@[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*$/;
     if (input_correo.value.match(formato_correo)){
