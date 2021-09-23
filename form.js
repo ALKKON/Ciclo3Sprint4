3d094282982a54d94c1d9d2d673a919384738dbc
Ing. Germán Olaya <86852832+andrengineer@users.noreply.github.com>
2021-09-23 00:02:30 -0500

    commit inicio proyecto

--- form.js
+++ form.js
@@ -1,16 +1,8 @@
+// Validar formulario de registro
 
-// Validar correo electrónico
-function checkCorreo(input_correo) {
-    let formato_correo = /^[a-zA-Z0-9À-ÿ.+=_~-]+@[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*$/;
-    if (input_correo.value.match(formato_correo)){
-        return true;
-    } else {
-        return false;
-    }
-}
 
-//module.exports = checkNombre;
-//module.exports = checkTelefono;
-//module.exports = checkDir;
+module.exports = checkNombre;
+module.exports = checkTelefono;
+module.exports = checkDir;
 module.exports = checkCorreo;
-//module.exports = checkContrasena;
+module.exports = checkContrasena;
