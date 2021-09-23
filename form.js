cab60805d68d7a05a816c148d168d3f92074b28a
Pabon217 <ivanpabon217@gmail.com>
2021-09-23 16:10:27 -0500

    function checkDir

--- form.js
+++ form.js
@@ -1,5 +1,17 @@
 // Validar formulario de registro
 
+function checkDir (valor){
+
+    console.log(valor)
+    let direccion_valida =/^[a-zA-Z0-9#-\s]{5,50}$/;
+
+    if (direccion_valida.test(valor.value) ){
+        return true;
+    }else{
+        return false;
+    }
+
+}
 
 module.exports = checkNombre;
 module.exports = checkTelefono;
