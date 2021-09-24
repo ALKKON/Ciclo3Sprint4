b7c5d93c7b7e0234cde667b65e2a8d50180cbe60
Pabon217 <ivanpabon217@gmail.com>
2021-09-23 19:09:37 -0500

    function contrasena

--- form.js
+++ form.js
@@ -1,4 +1,12 @@
 // Validar formulario de registro
+function checkContrasena (valor){
+    let contrasena_valida = /^[a-zA-Z0-9]{8,50}$/;
+    if (contrasena_valida.test(valor.value) ){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 
 module.exports = checkNombre;
