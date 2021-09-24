1732937069cdc784848ae5dec30dc651035e304e
Ing. Germán Olaya <86852832+andrengineer@users.noreply.github.com>
2021-09-24 15:28:13 -0500

    Commit final script form

--- form.js
+++ form.js
@@ -1,20 +1,71 @@
+/**
+* Función del módulo form.js para validar el nombre de usuario
+* @param {object} inputNombre Objeto tipo input para nombre de usuario
+* @returns {boolean} Booleando de validación
+*/
+function checkNombre() {
+    let formatoNombre = /^\w+[^.\n\d]{4,30}$/;
+    if (formatoNombre.test(document.getElementById('nombre').value)) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 /**
- * Función del módulo form.js para validar correo electrónico
- * @param {object} input_correo Objeto tipo input para email
- * @returns {boolean} Booleano de validación
+ * Función del módulo form.js para validar el número de teléfono
+ * @param {object} inputTelefono Objeto tipo input para el teléfono
+ * @returns {boolean} Booleando de validación
  */
-function checkCorreo(input_correo) {
-    let formato_correo = /^[a-zA-Z0-9À-ÿ.+=_~-]+@[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*$/;
-    if (input_correo.value.match(formato_correo)){
+function checkTelefono() {
+    let formatoTelefono = /^[0-9]{7}$/;
+    if (formatoTelefono.test(document.getElementById('telefono').value)) {
         return true;
     } else {
         return false;
     }
 }
 
-//module.exports = checkNombre;
-//module.exports = checkTelefono;
-//module.exports = checkDir;
-module.exports = checkCorreo;
-//module.exports = checkContrasena;
+/**
+ * Función del módulo form.js para validar la dirección
+ * @param {object} inputDireccion Objeto tipo input para la dirección
+ * @returns {boolean} Booleano de validación
+ */
+function checkDir() {
+    let formatoDireccion = /^\S[a-zA-Z0-9\-\s\#][^\n]{5,50}$/;
+    if (formatoDireccion.test(document.getElementById('direccion').value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Función del módulo form.js para validar correo electrónico
+ * @param {object} inputCorreo Objeto tipo input para email
+ * @returns {boolean} Booleano de validación
+ */
+function checkCorreo() {
+    let formatoCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*$/;
+    if (formatoCorreo.test(document.getElementById('correo').value)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Función del módulo form.js para validar la contraseña
+ * @param {objecto} inputClave Objeto de tipo input para la contraseña
+ * @returns {boolean} Booleando de validación
+ */
+function checkContrasena() {
+    let formatoClave = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[\w\s][^\n]{8,}$/;
+    if(formatoClave.test(document.getElementById('clave').value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {checkDir, checkCorreo, checkTelefono, checkNombre};
