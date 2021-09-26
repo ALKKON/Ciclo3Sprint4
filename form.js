679b3b443541f165055cdc5f8ef0516bc3950702
kate88-j <angiegamba54@gmail.com>
2021-09-26 16:55:57 -0500

    función validar nombre

--- form.js
+++ form.js
@@ -1,8 +1,25 @@
-// Validar formulario de registro
+// Validar formulario de "Nombre"
+const formulario= document.getElementById('form-registro');
+console.log(formulario);
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log(checkNombre(document.getElementById('nombre')))
+});
 
+function checkNombre(valor) {
 
-module.exports = checkNombre;
-module.exports = checkTelefono;
-module.exports = checkDir;
-module.exports = checkCorreo;
-module.exports = checkContrasena;
+    console.log(valor)
+    let nombre_valido =/^[a-zA-Z]+$/;
+
+    if (nombre_valido.test(valor.value) ){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//module.exports = checkNombre;
+//module.exports = checkTelefono;
+//module.exports = checkDir;
+// module.exports = checkCorreo;
+//module.exports = checkContrasena;
