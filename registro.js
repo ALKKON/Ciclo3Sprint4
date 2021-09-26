bc471225b0107d2dc05fd9a0a694d8b3c50b2026
Ing. Germán Olaya <86852832+andrengineer@users.noreply.github.com>
2021-09-26 12:19:10 -0500

    Esquema registro.js Sprint 3

---
+++ registro.js
@@ -0,0 +1,19 @@
+function agregarRegistro() {
+
+    console.log();
+}
+
+function ordenarArreglo(arreglo) {
+
+    console.log(arreglo);
+    return arreglo;
+}
+
+function filtrarCorreo(arreglo) {
+    let correos = [];
+
+    console.log(correos);
+    return correos;
+}
+
+module.exports = { agregarRegistro,  ordenarArreglo, filtrarCorreo }
